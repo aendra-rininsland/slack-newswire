b94719c62f6e3285e7294bd2f0a66b21dacb7559
Ændrew Rininsland <aendrew@aendrew.com>
2015-08-31 07:29:31 +0100

    Initial.

--- index.js
+++ index.js
@@ -8,7 +8,10 @@
  */
 
 // module dependencies
-var AWS = require('aws-sdk');
+var xml2js = require('xml2js');
+var xpath = require('xml2js-xpath');
+var scale = require('d3-scale');
+var format = require('util').format;
 
 /**
  * The handler function.
@@ -17,5 +20,87 @@ var AWS = require('aws-sdk');
  * @param {object}  context     The AWS Lambda execution context.
  */
 exports.handler = function(event, context) {
-    context.succeed();
+  /**
+   * Parses an old NewsML XML file (Press Association)
+   */
+  function parseArticle(article, type, priority) {
+    var color = scale.linear().domain([8, 3, 1]).range(['green', 'yellow', 'red']);
+    var headline = xpath.evalFirst(article, '//headline');
+    var body = xpath.evalFirst(article, '//body');
+    var bodyCopy, excerpt, byline, link, newsitem;
+    
+    if (body.hasOwnProperty('body.content')) { // PA
+      excerpt = bodyCopy = body['body.content'][0].p[0];
+      bodyCopy = body['body.content'][0].p.join('\n');
+      byline = xpath.evalFirst(article, '//byline').replace('By ', '');
+      link = 'https://www.pressassociation.com/';
+      newsitem = xpath.evalFirst(article, '//newsitemid');
+    } else { // Reuters
+      excerpt = body.p[0];
+      bodyCopy = body.p.join('\n');
+      // var authors = body.p[body.p.length - 1];
+      byline = 'Thomson Reuters'; // TODO write some regex to extract reporter's name.
+      link = 'http://about.reuters.com/';
+      newsitem = article.$.guid.split(':')[2].replace('newsml_', '');
+    }
+    
+    return {
+      fallback: format('%s [%d] -- %s', headline, priority, excerpt),
+      color: color(priority),
+      title: format('%s [%d]', headline, priority),
+      pretext: priority <= 3 ? '@everyone' : '',
+      text: bodyCopy,
+      author_name: byline,
+      author_link: link,
+      fields: [
+        {
+          title: 'slugline',
+          value: xpath.evalFirst(article, '//slugline', true)
+        },
+        {
+          title: 'Methode Name',
+          value: undefined
+        },
+        {
+          title: 'News Item ID',
+          value: newsitem
+        }
+      ]
+    };
+  }
+  
+  xml2js.parseString(event.body, {normalizeTags: true}, function(err, xml){
+    var type;
+    var payload = {
+      text: String(),
+      attachments: []
+    };
+  
+    var articles = xpath.find(xml, '//newsitem');
+    var priority = xpath.evalFirst(xml, '//priority');
+    var metadataProperties = xpath.find(xml, '//nimetadata/property');
+    var methode = metadataProperties.filter(function(v){
+      return v.$.FormalName === 'NIMethodeName';
+    })[0].$.Value;
+      
+    if (xml.hasOwnProperty('newsmessage')) { // Reuters, expectedly, uses the modern version.
+      type = 'Reuters';
+    } else if (xml.hasOwnProperty('newsml')) { // PA, regrettably, does not.
+      type = 'PA';
+      priority = priority.$.FormalName;
+    } else {
+      throw 'Not valid NewsML';
+    }
+    payload.type = type;
+    
+    if (articles.length > 0) {
+      articles.forEach(function(article){
+        var parsed = parseArticle(article, type, priority);
+        parsed.fields[1].value = methode;
+        payload.attachments.push(parsed);
+      });
+    }
+    
+    context.succeed(payload);
+  });  
 };
--- test/test.js
+++ test/test.js
@@ -1,17 +1,19 @@
 'use strict';
 
 /**
- * Test suite for POSTs newswire stories to Slack via an incoming webhook.
- * 
- * @author Ændrew Rininsland      <aendrew@aendrew.com>
- * @since  30 Aug. 2015
- */
+* Test suite for POSTs newswire stories to Slack via an incoming webhook.
+* 
+* @author Ændrew Rininsland      <aendrew@aendrew.com>
+* @since  30 Aug. 2015
+*/
 
 // module dependencies
 var chai = require('chai'),
     sinon = require('sinon'),
     sinonChai = require('sinon-chai'),
+    fs = require('fs');
     context = require('aws-lambda-mock-context');
+var assert = chai.assert;
 
 // Use should flavour for Mocha
 chai.should();
@@ -20,18 +22,71 @@ chai.use(sinonChai);
 var index = require('../');
 
 describe('SlackNewswire', function() {
-    
-    it('Should call the succeed method', function(done) {
-        index.handler({hello: 'world'}, context());
-
-        context.Promise
-            .then(function() {
-                // succeed() called
-                done();
-            })
-            .catch(function(err) {
-                // fail() called
-                done(err);
-            });
+  var pressAssociation = fs.readFileSync('test/data/press-association_mock.xml', 'UTF-8');
+  var reuters = fs.readFileSync('test/data/reuters_mock.xml', 'UTF-8');
+  
+  it('Should parse PA feeds', function(done) {
+    index.handler({body: pressAssociation}, context());
+    context.Promise
+    .then(function(data) { // succeed() called
+      assert(data.type === 'PA', 'Ensure type is PA.');
+      assert(data.attachments.length === 1, 'There is one article.')
+      
+      var attachment = data.attachments[0];
+      assert.propertyVal(attachment, 'fallback', 'HEADLINE HEADLINE [4] -- BODY COPY BODY COPY');
+      assert.propertyVal(attachment, 'color', '#cce600');
+      assert.propertyVal(attachment, 'title', 'HEADLINE HEADLINE [4]');
+      assert.propertyVal(attachment, 'pretext', '');
+      assert.propertyVal(attachment, 'text', 'BODY COPY BODY COPY\nends');
+      assert.propertyVal(attachment, 'author_name', 'FIRSTNAME LASTNAME, Press Association');
+      assert.propertyVal(attachment, 'author_link', 'https://www.pressassociation.com/');
+      //assert.propertyVal(attachment, 'author_icon', '');
+      assert(attachment.fields.length === 3, 'Ensure there are two fields');
+      assert.propertyVal(attachment.fields[0], 'title', 'slugline');
+      assert.propertyVal(attachment.fields[0], 'value', 'POLICE Collision');
+      assert.propertyVal(attachment.fields[1], 'title', 'Methode Name');
+      assert.propertyVal(attachment.fields[1], 'value', '28PACOLLISION1');
+      assert.propertyVal(attachment.fields[2], 'title', 'News Item ID');
+      assert.propertyVal(attachment.fields[2], 'value', 'PA-HHH-POLICE-Collision');
+      done();
+    })
+    .catch(function(err) { // fail() called
+      done(err);
     });
+  });
+  
+  it('Should parse Reuters feeds', function(done) {
+    index.handler({body: reuters}, context());
+    
+    context.Promise
+    .then(function(data) { // succeed() called
+      // console.dir(data);
+      assert(data.type === 'Reuters', 'Ensure type is Reuters.');
+      assert(data.attachments.length === 1, 'There is one article.')
+      
+      var attachment = data.attachments[0];
+      assert.propertyVal(attachment, 'fallback', 'HEADLINE HEADLINE HEADLINE [2] -- BODY COPY');
+      assert.propertyVal(attachment, 'color', '#ff8000');
+      assert.propertyVal(attachment, 'title', 'HEADLINE HEADLINE HEADLINE [2]');
+      assert.propertyVal(attachment, 'pretext', '@everyone');
+      assert.propertyVal(attachment, 'text', 'BODY COPY\n(Reporting by FIRSTNAME LASTNAME; writing by FIRSTNAME LASTNAME; editing by FIRSTNAME LASTNAME)');
+      assert.propertyVal(attachment, 'author_name', 'Thomson Reuters');
+      assert.propertyVal(attachment, 'author_link', 'http://about.reuters.com/');
+      //assert.propertyVal(attachment, 'author_icon', '');
+      assert(attachment.fields.length === 3, 'Ensure there are two fields');
+      assert.propertyVal(attachment.fields[0], 'title', 'slugline');
+      assert.propertyVal(attachment.fields[0], 'value', 'PLACE-TOPIC/SUBJECT (URGENT)');
+      assert.propertyVal(attachment.fields[1], 'title', 'Methode Name');
+      assert.propertyVal(attachment.fields[1], 'value', '28RTURGENT');
+      assert.propertyVal(attachment.fields[2], 'title', 'News Item ID');
+      assert.propertyVal(attachment.fields[2], 'value', 'ABCDEFGH');
+      done();
+    })
+    .catch(function(err) {
+      // fail() called
+      done(err);
+    });
+  });
+  
+  
 });
